cb9bcc30e9cf535522cd678d0f25547ec25d088d
Kim <koboticss@github.com>
2016-03-11 12:28:18 -0500

    Added othe rfiles

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/kim/baxter/ros_ws/src/baxter_dance/src
+# Build directory: /home/kim/baxter/ros_ws/src/baxter_dance/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ build/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/kim/baxter/ros_ws/src/baxter_dance/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
