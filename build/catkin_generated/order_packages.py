b16177fe8f5e101acfa73cb7205b005ed352a478
Kim <koboticss@github.com>
2016-03-21 16:56:21 -0400

    Removed build and devel folders

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/kim/baxter/ros_ws/src/baxter_dance/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/indigo".split(';') if "/opt/ros/indigo" != "" else []
